4c384575308a4c3d02d06477176a9b64b48fa83d
Harshit Siwach <37680927+harshitsiwach@users.noreply.github.com>
2025-01-04 03:16:33 +0530

    changed thumbnail for base neko

--- src/components/ui.tsx
+++ src/components/ui.tsx
@@ -88,7 +88,7 @@ const Ui: React.FC<Ui> = ({ isLoading, selectedGame, onGameSelect }) => {
               <div className="mt-8">
               <div className="flex items-center justify-between rounded-xl bg-[#0A1238] p-4">
                 <div className="flex items-center gap-4">
-                  <div className="h-12 w-12 rounded-lg bg-blue-500/20 bg-center  bg-cover bg-[url('/gameimg/cat.png')]"></div>
+                  <div className="h-12 w-12 rounded-lg bg-blue-500/20 bg-center  bg-cover bg-[url('/gameimg/bASE_NEKO_lg.png')]"></div>
                   <span className="font-semibold">Base Neko</span>
                 </div>
                 <button className="rounded-lg border border-[#4241ff] px-6 py-2 text-sm font-medium text-[#4241ff]"
