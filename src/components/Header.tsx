eaf13056d4cceb25bfb5b2f4ef70941878b54e62
NikoSAN02 <dineshbohra@gmail.com>
2025-02-03 04:24:02 +0530

    Updated header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -82,16 +82,14 @@ function Header() {
         {/* User Details */}
         <div className="text-right">
           {/* Base Name or Address */}
-          {account?.address ? (
+          {account?.address && (
             <p className="text-sm font-semibold">
               <Name
                 address={account.address}
                 chain={base}
-                fallback={shortenAddress(account.address)} // Fallback to address if no base name
+                fallback={shortenAddress(account.address)}
               />
             </p>
-          ) : (
-            <p className="text-sm font-semibold">No Address Found</p>
           )}
 
           {/* XP Display */}
